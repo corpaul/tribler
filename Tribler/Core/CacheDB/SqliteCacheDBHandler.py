4f31448e948377bbc0fe8bf05195805bc4510dbc
Cor-Paul Bezemer <c.bezemer@tudelft.nl>
2014-04-25 09:58:33 +0200

    simpler get_votes query

--- Tribler/Core/CacheDB/SqliteCacheDBHandler.py
+++ Tribler/Core/CacheDB/SqliteCacheDBHandler.py
@@ -1970,8 +1970,8 @@ class VoteCastDBHandler(BasicDBHandler):
 
             if channel_ids:
                 def get_votes():
-                    # sql = "SELECT * FROM (" + " UNION ALL ".join("SELECT %s, vote FROM ChannelVotes WHERE channel_id = %s" % (cid, cid) for cid in channel_ids) + ")"
-                    sql = "SELECT * FROM ChannelVotes WHERE channel_id = %s" % choice(channel_ids)
+                    sql = "SELECT * FROM (" + " UNION ALL ".join("SELECT %s, vote FROM ChannelVotes WHERE channel_id = %s" % (cid, cid) for cid in channel_ids) + ")"
+                    # sql = "SELECT * FROM ChannelVotes WHERE channel_id = %s" % choice(channel_ids)
                     print sql
                     positive_votes = {}
                     negative_votes = {}
